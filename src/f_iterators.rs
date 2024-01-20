8aa1795c70ba8c40e46994a8880c0497abf14719
Polka.Dom <polkadotdom@gmail.com>
2024-01-20 15:21:38 -0500

    Iterators

--- src/f_iterators.rs
+++ src/f_iterators.rs
@@ -11,16 +11,15 @@
 /// This function takes an iterator of u32 values, squares each value, and returns the sum of the
 /// squares. You may assume that no individual square, nor the entire sum, overflows the u32 type.
 pub fn sum_of_squares(vals: impl Iterator<Item = u32>) -> u32 {
-	todo!()
+	vals.map(|x| x*x).sum()
 }
 
 /// This function takes an iterator of i32 values, calculates the absolute value of each, and throws
 /// away any values that are greater than 100. The remaining positive values are returned as an
 /// iterator of u32s.
 pub fn bounded_absolute_values(vals: impl Iterator<Item = i32>) -> impl Iterator<Item = u32> {
-	// You should remove the following line (and this comment). It is just there because the
-	// compiler doesn't allow todo!() when the return type is impl Trait
-	Vec::new().into_iter()
+	//could you filter map but I like the one liner
+	vals.map(|x| x.abs() as u32).filter(|x| *x <= 100)
 }
 
 // We allow `unused_mut` only so that there is no build warning on the starter code.
@@ -32,11 +31,10 @@ pub fn bounded_absolute_values(vals: impl Iterator<Item = i32>) -> impl Iterator
 ///
 /// If the input iterator is empty, return None
 /// If there are fewer than n even values left in the input, return as many as possible
-#[allow(unused_mut)]
 pub fn first_n_even(mut vals: impl Iterator<Item = u32>) -> Option<impl Iterator<Item = u32>> {
-	// You should remove the following line (and this comment). It is just there because the
-	// compiler doesn't allow todo!() when the return type is impl Trait
-	Some(Vec::new().into_iter())
+	let n = vals.next()?;
+	let evens = vals.filter(|x| x % 2 == 0).take(n as usize);
+	Some(evens)
 }
 
 /// Return an "infinite" iterator that yields the squares of the whole numbers.
@@ -44,9 +42,17 @@ pub fn first_n_even(mut vals: impl Iterator<Item = u32>) -> Option<impl Iterator
 ///
 /// The iterator should be bounded only by the u32 type, not by your code
 pub fn square_whole_numbers() -> impl Iterator<Item = u32> {
-	// You should remove the following line (and this comment). It is just there because the
-	// compiler doesn't allow todo!() when the return type is impl Trait
-	Vec::new().into_iter()
+	let mut ind: u32 = 0;
+	std::iter::from_fn(move || {
+		let sq = ind.checked_pow(2);
+		match sq {
+			Some(res) => {
+				ind = ind + 1; //no need to check, as u32 overflow will happen first
+				Some(res)
+			},
+			_ => None
+		}
+	})
 }
 
 /// An iterator that generates the Fibonacci sequence.
@@ -61,8 +67,19 @@ pub struct Fibonacci {
 impl Iterator for Fibonacci {
 	type Item = u32;
 
-	fn next(&mut self) -> Option<u32> {
-		todo!()
+	fn next(&mut self) -> Option<Self::Item> {
+		//get next value
+		let next_val = match (self.prev, self.prev_prev) {
+			(Some(prev), Some(prev_prev)) => prev.checked_add(prev_prev),
+			(Some(prev), None) => Some(1),
+			_ => Some(0),
+		};
+		//slide values over
+		if let Some(val) = next_val {
+			self.prev_prev = self.prev;
+			self.prev = Some(val);
+		}
+		next_val
 	}
 }
 
@@ -70,13 +87,13 @@ impl Iterator for Fibonacci {
 /// On a scale from 0 - 255, with zero being extremely easy and 255 being extremely hard,
 /// how hard did you find this section of the exam.
 pub fn how_hard_was_this_section() -> u8 {
-	todo!()
+	200
 }
 
 /// This function is not graded. It is just for collecting feedback.
 /// How much time (in hours) did you spend on this section of the exam?
 pub fn how_many_hours_did_you_spend_on_this_section() -> u8 {
-	todo!()
+	2
 }
 
 #[cfg(test)]
